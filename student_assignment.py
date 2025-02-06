d916614ce98545d1001390249390e1238864ed24
LiangTsaoWei <153880969+LiangTsaoWei@users.noreply.github.com>
2025-02-06 17:41:20 +0800

    Update student_assignment.py

--- student_assignment.py
+++ student_assignment.py
@@ -7,6 +7,14 @@ q2_pdf = "勞動基準法.pdf"
 
 
 def hw02_1(q1_pdf):
+    loader = PyPDFLoader(q1_pdf)
+    docs = loader.load()
+    
+    # 使用 CharacterTextSplitter 將文本切割成塊
+    text_splitter = CharacterTextSplitter(chunk_overlap=0)
+    chunks = text_splitter.split_documents(docs)
+    if chunks:
+        print(chunks[-1])
     pass
 
 def hw02_2(q2_pdf):
