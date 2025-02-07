69103fbb780568576b52247406c93b20b66926a7
LiangTsaoWei <153880969+LiangTsaoWei@users.noreply.github.com>
2025-02-07 15:06:47 +0800

    Update student_assignment.py

--- student_assignment.py
+++ student_assignment.py
@@ -1,6 +1,7 @@
 from langchain_community.document_loaders import PyPDFLoader
 from langchain_text_splitters import (CharacterTextSplitter,
                                       RecursiveCharacterTextSplitter)
+import re
 
 q1_pdf = "OpenSourceLicenses.pdf"
 q2_pdf = "勞動基準法.pdf"
@@ -18,4 +19,29 @@ def hw02_1(q1_pdf):
     pass
 
 def hw02_2(q2_pdf):
+    loader = PyPDFLoader(q2_pdf)
+    docs = loader.load()
+    
+    # 合併所有頁面的文本
+    full_text = "\n".join([doc.page_content for doc in docs])
+    
+    # 使用 RecursiveCharacterTextSplitter 將文本切割成塊
+    text_splitter = RecursiveCharacterTextSplitter(
+        chunk_size=6,
+        chunk_overlap=0,
+        separators = [r"第\s*\d+\s*條"],
+    )
+    chunks = text_splitter.split_text(full_text)
+    
+    # 打印 chunks 的數量
+    print(f"Number of chunks: {len(chunks)}")
+
+    if chunks:
+        return (len(chunks))
     pass
+
+#if __name__ == "__main__":
+#     # last_chunk = hw02_1(q1_pdf)
+#     # print(last_chunk)
+#     chunk_count = hw02_2(q2_pdf)
+#     print(chunk_count)
